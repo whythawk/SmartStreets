461cf45c3409ea913ef28980f9c9844a02f69b44
toby <toby@localhost.localdomain>
2015-06-14 21:51:39 +0100

    Make into package for wsgi

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SmartStreets",
+    version="0.1",
+    author="Toby Dacre",
+    author_email="toby.junk@gmail.com",
+    description=(''),
+    license="",
+    keywords="",
+    url="",
+    packages=['smartstreets'],
+    long_description='',
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Programming Language :: Python :: 2.7',
+    ],
+)
